3cd02f911d82030d7c0763b5544c96f699a6a935
Jesse Wenger <wengerj@gmail.com>
2021-03-10 19:20:33 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid 
-  attr_accessor :name
   include Dance
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
 end
