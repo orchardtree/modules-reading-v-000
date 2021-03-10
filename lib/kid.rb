7aefd6d69d56c15cbd544a757b83ca0d28734345
Jesse Wenger <wengerj@gmail.com>
2021-03-10 20:22:37 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-require_relative './fancydance.rb'
+require_relative './fancy_dance.rb'
 
 class Kid 
   extend FancyDance::ClassMethods
